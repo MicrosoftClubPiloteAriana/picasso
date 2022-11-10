6ab93676195152dbcd6b070ad1ade747c0db5f44
Mino260806 <dmezghanni@gmail.com>
2022-11-11 00:37:58 +0100

    added comments and fixed a minor bug

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bezier import CubicBezier
 from paintassistant import PaintAssistant
 
 
+# We need to scale the original coordinates to fit the screen
 c = lambda x: x / 3
 
 SCREEN_WIDTH = c(1200)
@@ -11,12 +12,26 @@ SCREEN_HEIGHT = c(1434)
 
 
 def moveto(x, y):
+    """
+    Moves to a certain point without drawing a line
+    """
     penup()
-    goto(c(522.00), c(638.67))
+    goto(c(x), c(y))
     pendown()
 
 
 def bezier_cubic(startx, starty, x1, y1, x2, y2, endx, endy):
+    """
+    Draws a Bézier curve on the screen
+    :param startx: the x-coordinate of the point at the start
+    :param starty: the y-coordinate of the point at the start
+    :param x1: the x-coordinate of the control point for the start of the curve
+    :param y1: the y-coordinate of the control point for the start of the curve
+    :param x2: the x-coordinate of the control point for the end of the curve
+    :param y2: the y-coordinate of the control point for the end of the curve
+    :param endx: the x-coordinate of the point at the start
+    :param endy: the x-coordinate of the point at the start
+    """
     curve = CubicBezier(startx, starty, x1, y1, x2, y2, endx, endy).calc_curve()
     for x, y in zip(*curve):
         goto(c(x), c(y))

42e09dede7c64b75785decaf194a43733c076277
Mino260806 <dmezghanni@gmail.com>
2022-11-11 01:00:18 +0100

    updated drawing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from paintassistant import PaintAssistant
 
 
 # We need to scale the original coordinates to fit the screen
-c = lambda x: x / 3
+c = lambda x: x / 2.5
 
 SCREEN_WIDTH = c(1200)
 SCREEN_HEIGHT = c(1434)
@@ -47,6 +47,17 @@ if __name__ == '__main__':
         moveto(522.00,638.67)
         bezier_cubic(522.00,638.67, 522.00,638.67, 652.00,904.67, 522.00,938.67)
         bezier_cubic(522.00,938.67, 390.67,938.00, 522.67,638.00, 522.67,638.00)
+
+    with assistant.fill("#8f8c8a"):
+        moveto(544.00,684.00)
+        bezier_cubic(544.00,684.00, 544.00,684.00, 556.62,715.50, 556.62,715.50)
+        bezier_cubic(556.62,715.50, 556.62,715.50, 507.67,813.67, 532.00,881.00)
+        bezier_cubic(532.00,881.00, 483.67,832.67 ,544.00,684.33, 544.00,684.33)
+
+    with assistant.fill("#aca9a6"):
+        moveto(524.00,640.67)
+        bezier_cubic(524.00,640.67, 524.00,640.67, 447.33,836.67, 493.50,930.50)
+        bezier_cubic(493.50,930.50, 413.33,882.67, 522.53,640.58, 522.53,640.58)
 
     s.mainloop()
 
